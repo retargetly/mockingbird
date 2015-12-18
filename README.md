da61c190397a796aaf0cdbcbc395bb260dd60416
Federico <federico@retargetly.com>
2015-12-18 17:53:19 -0300

    Added popup functionality to the plugin. Now the developers can configure whether to show different type of popups, and even block users navigation if desired.

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 ![mockingbird](mockingbird-logo.png?raw=true "Mockingbird")
-# Mockingbird (v1.0.1)
+# Mockingbird (v1.0.2)
 Detect ad blockers and take actions with a simple standalone script.
 ----------------------
-This is an on-line fiddle where you can play with the code: http://jsfiddle.net/retargetly/9vsha32h/5/
+This is an on-line fiddle where you can play with the code: http://jsfiddle.net/retargetly/9vsha32h/6/
 
 And this is the on-line web example preview: http://dmp.retargetly.com/mockingbird
 
 Also check the example folder, you can download it and test it on your local machine.
+
+##Description
+----------------------
+
+Mockingbird is an open source plugin for detecting users with ad blockers that navigate your site, and show them different messages, or even static advertising using your own servers as the ad server.
 
 Works on the following browsers
 ----------------------
@@ -20,7 +25,7 @@ Works on the following browsers
 
 Just download mockingbird.js (or mockingbird.min.js) and add it to your website. Then execute the only method "adsBlocked" with its parameters upon your desire.
 
-##Code example
+##Javascript code example
 ---------------------
 ```javascript
 if(mockingbird)
@@ -37,10 +42,25 @@ if(mockingbird)
 			// link: 'http://retargetly.com/#top2',
 			selector: '.container2,.container3',
 			msg:'We live from Ads, help us to protect our content by disabling the ad blocker for our site.',
-			msgClass:'msg',
+			classes: {
+				container:'cnt',
+				msg:'msg',
+			},
 			replaceContents: true,
 		}],
-		
+		popup: {
+			title: 'WARNING',
+			msg: '<strong>Please</strong> disable your ad blocker',
+			buttonLabel: 'Keep browsing',
+			type: 'small',
+			classes: {
+				title: '',
+				msg: '',
+				container: '',
+				exitButton: ''
+			},
+			exitButton: true
+		},
 		handler: function() {
 			console.log("Ad Blocker detected")
 		}
@@ -48,7 +68,22 @@ if(mockingbird)
 }
 ```
 
-## Explanation
+##HTML code example
+
+```html
+	<!-- this example uses google DFP tags -->
+	<div class="container1" style="margin:0 auto;width:728px;height:90px;background:#000;">
+		<div id="div-gpt-ad-1450125646903-0" style="height:90px; width:728px;">
+			<script type='text/javascript'>
+			googletag.cmd.push(function() { googletag.display('div-gpt-ad-1450125646903-0'); });
+			</script>
+		</div>
+		<!-- if the adblocker blocked ads, then this container (.container1) will be emptied and then will be loaded with whatever message or ad you configured -->
+	</div>
+```
+
+
+## Method Properties
 
 Anywhere on the html of your site you can initiate mockingbird script with the method adsBlocked. It receives an object which can have the following properties:
 
@@ -60,22 +95,40 @@ This is an array of Ads containers. Each ad container will be an object with the
     * link: [string] (optional) - Default: null -> full url of the ad destination
     * title: [string] (optional) - Default: 'link' -> title of the <a> tag
     * replaceContents: [boolean] (optional) - Default: true -> will replace all the content of the containers that match the selector
-    * msg: [string] (optional) - Default: null -> The message to be displayed if ad blocker enabled
-    * msgClass: [string] (optional) - Default: null -> The class of the span that will hold the message
+    * msg: [string] (optional) - Default: null -> The message to be displayed if ad blocker enabled. Also accepts html markup string.
+    * classes: [obj] (optional) - Default: null -> Object with classes properties
+        * container: [string] (optional) - Default: null -> The class that will be added to the containers matched in "selector" property if ads blocked
+        * msg: [string] (optional) - Default: null -> The class that will be added to the message if ads blocked
+
+* popup [object]
+This is the popup object that will define the popup behaviour, can be a full page popup or a small notification on the top of the page. It has the following properties:
+
+    * title: [string] (optional) - Default: null -> [Only for type:'full']. Is the title of the popup window. Also accepts html markup string.
+    * buttonLabel: [string] (optional) - Default: 'Cancel' -> [Only for type:'full' and exitButton: true]. This is the button label. Also accepts html markup string.
+    * msg: [string] - Default: null -> This is the main message that will be displayed on the popup. Also accepts html markup string.
+    * type: [string] (optional) - Values: ['full','small'] - Default: 'small' -> The popup type. 'full' is a popup that occupies the whole page, 'small' is a small text on the top of the page
+    * exitButton: [boolean] (optional) - Default: true -> Enables/disables a button to close the popup
+    * classes: [object] (optional) - Default: null -> Object with classes properties
+        * title: [string] (optional) - Default: null -> [Only for type:'full']. Removes default css of popup title and adds the value as class
+        * msg: [string] (optional) - Default: null -> Removes default css of popup message and adds the value as class.
+        * container: [string] (optional) - Default: null -> Removes default css of popup container and adds the value as class
+        * exitButton: [string] (optional) - Default: null -> [Only for exitButton: true]. Removes default css of popup close button and adds the value as class
+
 
 * handler [function] (optional) - Default null -> Handler to be executed if user has ad blocker enabled
 
 You have to put either img and link properties, or msg property, one of them is mandatory for the plugin to work. If image is provided then link should be provided as well.
 
-## Description
+## How it works
 
 Mockingbird uses two different ways for detecting ad block. First by containers with common ads classes, and then it falls back into a request to a local fake url with many common adds tags.
 
 ## Recomendations
 
-Download the file and change its name from mockingbird to something else so ad blockers won't block the load of the script.
+Download the file and change its name from mockingbird to something else so ad blockers won't block the load of the script. Also you can rename the method "adsBlocked" to anything else inside mockingbird.js and then rename it also on your html code.
 
 ## Next updates
 
-* Add parameter to show popup with a message, and have the possibility to not let the user keep browsing on the site.
+* New optional parameter 'redirect URL' so, if present, all traffic with ad blockers enabled goes to that URL until gets ad blocker disabled.
+* Add alternative ad server integration to keep showing ads to ad blocked traffic, on a similar way that DFP, Atlas or OpenX works where you upload campaigns and creativities, but of course not blocked by ad blockers.
 * Integration with Retargetly's DMP to get stats about traffic with ad blockers, impressions and clicks counters served to those users, a/b testing of ads and messages, possibility to change messages whithin the platform without changing site code.
--- mockingbird.js
+++ mockingbird.js
@@ -12,15 +12,16 @@ var mockingbird = (function () {
     		if(adb == -1 || (typeof obj.msg == "undefined" && typeof obj.handler != "function"))
     			return
 
-    		var s_fn = null
+    		var fn = null
 
-    		var fn = obj.handler || null
+    		xqfn(obj.handler)
     		var containers = obj.containers || null
+    		var popup = obj.popup || null
     		var selector
 
     		if(containers)
     		{
-    			s_fn = function()
+    			fn = function()
 	    		{
 				    docReady(function(){
 		    			for(var i=0;i<containers.length;i++)
@@ -41,8 +42,11 @@ var mockingbird = (function () {
 						    		}
 						    		else if(typeof containers[i].msg === "string")
 						    		{
-						    			sp = createMsg(containers[i].msg,containers[i].msgClass)
+						    			sp = createMsg(containers[i].msg,containers[i].classes)
 						    		}
+
+						    		if(typeof containers[i].classes != "undefined" && typeof containers[i].classes.container != "undefined")
+						    			cs[j].className += ' '+containers[i].classes.container
 
 						    		if(sp)
 						    		{
@@ -54,23 +58,182 @@ var mockingbird = (function () {
 			    		}
 				    })
     			}
+
+    			xqfn(fn)
     		}
 
-    		if(adb == 1) //ad blocker detected
+    		if(popup)
     		{
-    			if(typeof s_fn == "function")
-					s_fn()
+    			fn = function() {
+    				if(typeof popup.msg == "undefined")
+    					return
+    				
+    				if(typeof popup.title == "undefined")
+    					popup.title = ''
 
-				if(typeof fn == "function")
-					fn()
+    				if(popup.type != 'full')
+    					popup.type = 'small'
+
+
+    				if(popup.type == 'small')
+    				{
+
+    					var div = cDiv({
+    						msg: popup.msg,
+    						exit: popup.exitButton,
+    						classes: popup.classes
+    					})
+
+    					div.style.zIndex = '999'
+    				}
+    				else
+    				{
+    					var div = cDiv({
+    						msg: popup.msg,
+    						exit: popup.exitButton,
+    						title: popup.title,
+    						label: popup.buttonLabel,
+    						classes: popup.classes
+    					})
+
+    					var transparent = document.createElement('div')
+
+    					transparent.setAttribute("style", "filter:progid:DXImageTransform.Microsoft.Alpha(opacity=50)");
+
+    					transparent.style.zIndex = '998'
+    					transparent.style.opacity = 0.5
+						transparent.style.position='fixed'
+						transparent.style.top='0'
+						transparent.style.right='0'
+						transparent.style.bottom='0'
+						transparent.style.left='0'
+						transparent.style.width='100%'
+						transparent.style.background='#000'
+    					
+    					var parent = document.createElement('div')
+    					parent.style.zIndex = '998'
+						parent.style.width='100%'
+						parent.style.height='100%'
+						parent.style.position='fixed'
+						parent.style.top='0'
+    					
+						div.style.margin='0 auto'
+						div.style.marginTop='15%'
+						div.style.border='1px solid #bababa'
+						div.style.background='#FFF'
+						div.style.textAlign='center'
+						
+						div.style.width='400px'
+						div.style.height='auto'
+
+    					transparent.appendChild(div)
+
+    					parent.appendChild(div)
+    					div = parent
+    					document.body.appendChild(transparent)
+    				}
+
+    				document.body.appendChild(div)
+    			}
+
+    			xqfn(fn)
     		}
-			else if(adb==0)
-			{
-				addCallback(s_fn)
-				addCallback(fn)
-			}
-
     	}
+    }
+
+    function cDiv(obj)
+    {
+    	var small = true
+    	if(typeof obj.title != "undefined")
+    		small = false
+
+    	if(typeof obj.exit == "undefined")
+    		obj.exit = true
+
+    	var div = document.createElement('div')
+		var html = ''
+    		
+    	var containerStyle,titleStyle,buttonStyle,msgStyle
+
+    	if(small)
+    	{
+			if(typeof obj.classes == "object" && typeof obj.classes.container == "string" && obj.classes.container != "")
+				containerStyle = ' class="'+(typeof obj.classes.container == "string"?obj.classes.container:'')+'"'
+			else
+				containerStyle = ' style="text-align:center;font-size: 14px !important;box-sizing: border-box;font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif;padding:8px;border: 1px solid transparent;border-radius: 0 0 4px 4px;color: #a94442;background-color: #f2dede;border-color: #ebccd1;"'
+
+			if(typeof obj.classes == "object" && typeof obj.classes.exitButton == "string" && obj.classes.exitButton != "")
+		    	buttonStyle = ' class="'+(typeof obj.classes.exitButton == "string"?obj.classes.exitButton:'')+'"'
+			else
+		    	buttonStyle = ' style="margin-left:8px;font-family: Verdana,sans-serif;color: #000;opacity: 0.4;text-shadow: 0 1px 0 #fff;float: right;text-decoration: none !important;line-height: 1.2;"'
+		    
+			if(typeof obj.classes == "object" && typeof obj.classes.msg == "string" && obj.classes.msg != "")
+		    	msgStyle = ' class="'+(typeof obj.classes.msg == "string"?obj.classes.msg:'')+'"'
+		    else
+		    	msgStyle = ''
+
+    		html += '<div style="max-width:600px;position:fixed;top:0;left:0;right:0;margin:0 auto">'
+	    		html += '<div'+containerStyle+'>'
+	    		if(obj.exit)
+	    			html += '<a href="javascript:;" onmouseover="this.style.opacity = 1" onmouseout="this.style.opacity = 0.4" onclick="var p = this.parentElement.parentElement.parentElement; var a = this.parentElement.parentElement; p.removeChild(a);"'+buttonStyle+'>×</a>'
+	    		html += '<span'+msgStyle+'>'+obj.msg+'</span>'
+	    		html += '</div>'
+    		html += '</div>'
+    	}
+    	else
+    	{
+
+    		if(typeof obj.classes == "object" && typeof obj.classes.container == "string" && obj.classes.container != "")
+				containerStyle = ' class="'+(typeof obj.classes.container == "string"?obj.classes.container:'')+'"'
+			else
+		    	containerStyle = ' style="top: 0;left: 0;max-width: 276px;padding: 1px;font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif;font-size: 14px;font-style: normal;font-weight: 400;line-height: 1.42857143;text-align: start;text-decoration: none;text-shadow: none;text-transform: none;letter-spacing: normal;word-break: normal;word-spacing: normal;word-wrap: normal;white-space: normal;background-color: #fff;background-clip: padding-box;border: 1px solid rgba(0,0,0,.2);border-radius: 6px;box-shadow: 0 5px 10px rgba(0,0,0,.2);"'
+
+    		if(typeof obj.classes == "object" && typeof obj.classes.title == "string" && obj.classes.title != "")
+				titleStyle = ' class="'+(typeof obj.classes.title == "string"?obj.classes.title:'')+'"'
+			else
+		    	titleStyle = ' style="padding: 8px 14px;margin: 0;font-size: 14px;background-color: #f7f7f7;border-bottom: 1px solid #ebebeb;border-radius: 5px 5px 0 0;"'
+
+			if(typeof obj.classes == "object" && typeof obj.classes.exitButton == "string" && obj.classes.exitButton != "")
+		    	buttonStyle = ' class="'+(typeof obj.classes.exitButton == "string"?obj.classes.exitButton:'')+'"'
+			else
+		    	buttonStyle = ' style="display: inline-block;padding: 6px 12px;margin-bottom: 8px;font-size: 14px;text-decoration:none;font-weight: 400;line-height:1.42857143;text-align:center;white-space: nowrap;vertical-align: middle;-ms-touch-action: manipulation;touch-action: manipulation;cursor: pointer;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;background-image: none;border: 1px solid transparent;border-radius: 4px;color: #fff;background-color: #5cb85c;border-color: #4cae4c;"'
+		    
+			if(typeof obj.classes == "object" && typeof obj.classes.msg == "string" && obj.classes.msg != "")
+		    	msgStyle = ' class="'+(typeof obj.classes.msg == "string"?obj.classes.msg:'')+'"'
+		    else
+		    	msgStyle = ''
+
+    		if(typeof obj.label == "undefined")
+    			obj.label = 'Cancel'
+
+	    	html += '<div'+containerStyle+'>'
+			    html += '<h3'+titleStyle+'>'+obj.title+'</h3>'
+			    html += '<div style="padding: 9px 14px;">'
+			    html += '<span'+msgStyle+'>'+obj.msg+'</span>'
+			    html += '</div>'
+
+		    if(obj.exit)
+		    	html += '<a href="javascript:;" title="'+obj.label+'" onclick="var p = this.parentElement.parentElement.parentElement; var a = this.parentElement.parentElement; var b = a.previousSibling; p.removeChild(a); p.removeChild(b);" '+buttonStyle+'>'+obj.label+'</a>'
+
+			html += '</div>'
+    	}
+
+    	div.innerHTML = html;
+		
+		return div.firstChild
+    }
+
+    function xqfn(fn)
+    {
+    	if(adb == 1) //ad blocker detected
+		{
+			if(typeof fn == "function")
+				fn()
+		}
+		else if(adb==0)
+		{
+			addCallback(fn)
+		}
     }
 
     function addCallback(fn)
@@ -107,12 +270,12 @@ var mockingbird = (function () {
     	return cs
     }
 
-    function createMsg(msg,msgClass)
+    function createMsg(msg,classes)
     {
     	var s = document.createElement('span')
 
-		if(typeof msgClass != "undefined")
-			s.className = msgClass
+		if(typeof classes != "undefined" && typeof classes.msg != "undefined")
+			s.className = classes.msg
 
 		s.innerHTML = msg
 
